3394ef5ceb3a821f0ab86875bb9f43bf27c3a7c2
Omtatah <murtallahomtatah@gmail.com>
2019-08-11 15:44:59 +0300

    define the dependancies needed to create my views

--- micasa/views.py
+++ micasa/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse,Http404
+from .models import *
+from django.contrib import messages
+from . forms import *
+from django.contrib.auth.models import User
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+    hoods = Hood.objects.all()
+    return render(request,'home.html',locals())
