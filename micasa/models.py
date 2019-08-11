9ac599e725f4927917ef4a903b1ed15bf33b6732
Omtatah <murtallahomtatah@gmail.com>
2019-08-11 14:41:10 +0300

    define the Location model

--- micasa/models.py
+++ micasa/models.py
@@ -5,3 +5,31 @@ from django.dispatch import receiver
 from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
+
+class Location(models.Model):
+    locations = (
+        ('Nairobi', 'Nairobi'),
+        ('Kiambu', 'Kiambu'),
+        ('Eastlands', 'Eastlands'),
+        ('Machakos', 'Machakos'),
+        ('Nakuru', 'Nakuru'),
+        ('Thika', 'Thika'),
+        ('Dubai', 'Dubai'),
+        ('New York', 'New York'),
+        ('Los Angeles', 'Los Angeles'),
+        ('Venice', 'Venice'),
+        ('Cairo', 'Cairo'),
+    )
+    name = models.CharField(max_length=65, choices=locations)
+
+
+
+    def save_loc(self):
+        self.save()
+
+    def delete_loc(self):
+        self.delete()
+
+
+    def __str__(self):
+        return self.name
